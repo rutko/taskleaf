59dd77095e6d8beded6f6c402541b24d2e65068e
rutko <t.222cm@gmail.com>
2019-07-15 22:32:31 +0900

    ✨ Add flash message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,5 +9,15 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
   def edit; end
+end
+private
+def task_params
+  params.require(:task).permit(:name, :description)
 end
