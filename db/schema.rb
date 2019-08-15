40c55c2ef3addcd4bfe10e9dc02163bc9d3e7334
rutko <t.222cm@gmail.com>
2019-08-15 23:54:31 +0900

    ✨ Add Task table to user_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_170757) do
+ActiveRecord::Schema.define(version: 2019_08_15_144929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_07_28_170757) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
