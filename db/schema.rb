704fc9d072211e4337683a336d38bf7cf63eec56
rutko <t.222cm@gmail.com>
2019-09-02 00:06:11 +0900

    ✨Add admin flag for User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_144929) do
+ActiveRecord::Schema.define(version: 2019_09_01_150110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_144929) do
     t.string "login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
