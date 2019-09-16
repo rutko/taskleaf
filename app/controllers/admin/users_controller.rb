517427161fbae2fedf15a3cea4fd82025742de3c
rutko <t.222cm@gmail.com>
2019-09-16 20:03:16 +0900

    🐛Fix password to .form-group indent

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,7 +19,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-    redirect_to admin_user_url(@user), notice: "ユーザ「#{@user.name}」を登録しました"
+      redirect_to admin_user_url(@user), notice: "ユーザ「#{@user.name}」を登録しました"
     else
       render :new
     end
@@ -37,7 +37,7 @@ class Admin::UsersController < ApplicationController
   def destory
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_user_url, notice: "ユーザ「#{@user.name}」を削除しました"
+    redirect_to admin_user_url, notice: "ユーザ「#{@user.name}」を削除しました。"
   end
 
   private
