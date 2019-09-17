d5e2a9d8332a0e04086c8ee42d81ca853ac14091
rutko <t.222cm@gmail.com>
2019-09-17 11:18:53 +0900

    ✨Add Logout form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    helper_method :currrent_user
+
+    private
+    def currrent_user
+        @current_user || = User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 end
